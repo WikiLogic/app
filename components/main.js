c7bb786911928314b2b54fbaf0de200f7effce14
Iain J McCallum <ijmccallum@hotmail.co.uk>
2017-01-02 19:44:00 -0500

    UI splite out

---
+++ components/main.js
@@ -0,0 +1,12 @@
+//rollup -> babel -> browser :)
+
+//import * as $ from '../node_modules/jquery/dist/jquery.js';
+//import $ from '../node_modules/jquery/dist/jquery.slim.js';
+
+//UI
+import alchemy from './alchemy/alchemy.js';
+
+//Yep, only one onload listener, but we only need one
+window.onload = function(){
+    alchemy.init();
+};
