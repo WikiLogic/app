070a100d26b4566178328c30d484d10d8ae12872
ijmccallum <ijmccallum@hotmail.co.uk>
2017-02-02 08:18:43 -0500

    trying to figure out the force

--- views/components/d3v4/force-simulation-config.js
+++ views/components/d3v4/force-simulation-config.js
@@ -2,17 +2,35 @@
 
 /* The magic number settings for the mysterious force layout.
  * https://github.com/d3/d3-force
+ * .force: adds a force of the given name, here's a refrence of the forces we can use: https://github.com/d3/d3/blob/master/API.md#forces-d3-force 
  */
 
 export default {
     default: function(d3, width, height){
         return d3.forceSimulation()
-            .force("link", d3.forceLink().iterations(4).id(function(d) { return d.id; }))
-            .force("charge", d3.forceManyBody().strength(-10) )
+            
+            //Link: creates a force between linked nodes    
+                //.forceLink() 
+            .force("link", d3.forceLink().id(function(d) { return d.id; }))
+            
+            //Charge: simluates the forces between nodes. Negative charge pushes away, positive attracts 
+            .force("charge", d3.forceManyBody().strength(1000) )
+            
+            //Collide: stops nodes overlapping
+                //.iterations - the more we add the less likley an overlap but the more computation required
             .force("collide", d3.forceCollide().radius(100).iterations(2) )
-            .force("center", d3.forceCenter(width / 2, height / 2) )
-            //force x & y are forces into the center (I think)
-            .force("x", d3.forceX())
-            .force("y", d3.forceY());
+
+            //Center: sets the center point to be the average of all nodes - does not affect the positioning of nodes relative to each other
+            //.force("center", d3.forceCenter(width / 2, height / 2) )
+
+            //ForceX & ForceY: creates a new force on all nodes towards the specified coords. Strength between 0 and 1
+            .force("x", d3.forceX(width / 2).strength(0.8))
+            .force("y", d3.forceY(height / 2).strength(0.8))
+            
+            //friction: slows the rate of travel from a node's original position
+            
+            //Alpha: reduces the strength of all forces on each tick
+            .alphaTarget(0).restart();
+            //.force("alpha", d3.alphaDecay());
     }
 }
